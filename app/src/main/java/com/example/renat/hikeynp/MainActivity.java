1b5e1c5305916ef939f1d7a1f116924b9058bce8
Renato Pavlekovic <renato.pavlekovic92@gmail.com>
2016-05-05 01:52:40 +0200

    expandableListView added to main activity
    added expandableListView for main activity including:
    -details
    -seasons
    -camping and lodging
    -shops, shuttles and tours
    maps and guidebooks
    food and drink
    -other activities

--- app/src/main/java/com/example/renat/hikeynp/MainActivity.java
+++ app/src/main/java/com/example/renat/hikeynp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.renat.hikeynp;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,17 +10,97 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ExpandableListView expandableListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expandableListView = (ExpandableListView)findViewById(R.id.exp_listview);
+        List<String> Headings = new ArrayList<String>();
+        List<String> L1 = new ArrayList<String>();
+        List<String> L2 = new ArrayList<String>();
+        List<String> L3 = new ArrayList<String>();
+        List<String> L4 = new ArrayList<String>();
+        List<String> L5 = new ArrayList<String>();
+        List<String> L6 = new ArrayList<String>();
+        List<String> L7 = new ArrayList<String>();
+
+        HashMap<String,List<String>> ChildList = new HashMap<String,List<String>>();
+        String heading_items[] = getResources().getStringArray(R.array.header_titles);
+        String l1[] = getResources().getStringArray(R.array.details);
+        String l2[] = getResources().getStringArray(R.array.seasons);
+        String l3[] = getResources().getStringArray(R.array.camping_and_lodging);
+        String l4[] = getResources().getStringArray(R.array.shops_shuttles_and_tours);
+        String l5[] = getResources().getStringArray(R.array.maps_and_guidebooks);
+        String l6[] = getResources().getStringArray(R.array.food_and_drink);
+        String l7[] = getResources().getStringArray(R.array.other_activities);
+
+        for(String title : heading_items){
+            Headings.add(title);
+        }
+
+        for(String title : l1){
+            L1.add(title);
+        }
+
+        for(String title : l2){
+            L2.add(title);
+        }
+
+        for(String title : l3){
+            L3.add(title);
+        }
+
+        for(String title : l4){
+            L4.add(title);
+        }
+
+        for(String title : l5){
+            L5.add(title);
+        }
+
+        for(String title : l6){
+            L6.add(title);
+        }
+
+        for(String title : l7){
+            L7.add(title);
+        }
+
+        ChildList.put(Headings.get(0), L1);
+        ChildList.put(Headings.get(1), L2);
+        ChildList.put(Headings.get(2), L3);
+        ChildList.put(Headings.get(3), L4);
+        ChildList.put(Headings.get(4), L5);
+        ChildList.put(Headings.get(5), L6);
+        ChildList.put(Headings.get(6), L7);
+
+        MyAdapter myAdapter = new MyAdapter(this,Headings,ChildList);
+        expandableListView.setAdapter(myAdapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
